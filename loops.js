fb2485f64097b274d55acbbefeeab5e7fbc7674d
sophieeng <seng@wesleyan.edu>
2024-09-05 09:46:00 -0700

    js practice

---
+++ loops.js
@@ -0,0 +1,33 @@
+// Function is called, the return value will end up in x
+let x = product(4, 3);
+
+function product(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+
+console.log(x)
+
+// Create an Object
+const person = new Object();
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+for (let i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+  }
+
+while (i < 10) {
+    text += "The number is " + i;
+    i++;
+  }
+
+do {
+    text += "The number is " + i;
+    i++;
+  }
+while (i < 10);
